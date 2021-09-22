d0e2e5830052bcb8e9973f9477f49ee55b26481d
eduardogomez97 <ryukedu86@gmail.com>
2021-09-22 15:02:26 +0200

    Ejercicio1 sección 3 PRIMEROS PASOS

---
+++ Ejercicio1PrimerosPasosOfuscado.java
@@ -0,0 +1,15 @@
+class Ejercicio1PrimerosPasosOfuscado {
+    public static void main(String[] args) {
+        final int _bc= 5;
+        int $ac = _bc*_bc;
+        System.out.println("El area de un cuadrado cuyo lado mide 5 cm es de: "+$ac+"cm");
+        final int _bt=10;
+        final int $ht=8;
+        int _$bht = (_bt*$ht)/2;
+        System.out.println("El area de un triangulo cuya base mide 10 cm y la altura de sus lados es de 8cm da una rea de: "+_$bht+"cm");
+        final int _br=20;
+        final int $hr=10;
+        int _$bhr = _br*$hr;
+        System.out.println("El area de un rectangulo cuya base mide 20 cm y la altura de sus lados es de 10cm da una rea de: "+_$bhr+"cm");
+    }
+}
--- HolaMundo.java
+++ HolaMundo.java
@@ -5,4 +5,3 @@ class HolaMundo{
         System.out.println(suma);
     }
 }
-
