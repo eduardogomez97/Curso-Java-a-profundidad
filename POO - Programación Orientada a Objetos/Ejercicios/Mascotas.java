6e8926965138d78fe3d24f428d2282b90e1cdf2a
eduardogomez97 <ryukedu86@gmail.com>
2021-09-27 23:27:19 +0200

    t4 inicio ejercicios

---
+++ POO - Programación Orientada a Objetos/Ejercicios/Mascotas.java
@@ -0,0 +1,31 @@
+class Mascotas {
+    
+    class Caballo{
+        String nombre;
+        int edad;
+        String raza;
+        String color;
+        String crin;
+        char sexo;
+    }
+    class Perro{
+        String nombre;
+        int edad;
+        String raza;
+        String color;
+        char sexo;
+    }
+    class Gato{
+        String nombre;
+        int edad;
+        String raza;
+        String color;
+        String caracter;
+        char sexo;
+    }
+
+    public static void main(String[] args) {
+    
+    }
+}
+
